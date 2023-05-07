1d66cbf9b55c238e3a4584137f9cae1cafe2988d
Alfredo Hernandez Jr <54375363+AlfredoHernandezJr@users.noreply.github.com>
2023-05-07 12:38:34 -0700

    Update JDB.java

--- Labs/Lab 6/JDB.java
+++ Labs/Lab 6/JDB.java
@@ -6,7 +6,7 @@ public class JDB {
 	
 	private static final String dbClassname = "com.mysql.cj.jdbc.Driver";
 	
-	private static final String CONNECTION = "jdbc:mysql://localhost:3306/test-database" ;
+	private static final String CONNECTION = "jdbc:mysql://localhost:3306/restaurant" ;
 	
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException
@@ -21,11 +21,27 @@ public class JDB {
 		p.put("password", "password1");
 		
 		Connection c = DriverManager.getConnection(CONNECTION,p);
-		
 		System.out.println("It works");
 		
-		c.close();		
-
+		// Select statement for sql query
+		String sql = "select * from foodmenu" ;
+		
+		PreparedStatement ps = null ;
+		ResultSet rs = null ;
+		
+		ps = c.prepareStatement(sql) ;
+		rs = ps.executeQuery() ;
+		
+		// Prints out the table's contents
+		while(rs.next())
+		{
+			String food = rs.getString("food") ;
+			System.out.println(food) ;
+		}
+		
+		// Closes all connections
+		c.close() ;		
+		ps.close() ;
+		rs.close() ;
 	}
-
 }
